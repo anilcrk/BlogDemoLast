dbfe80a5c474fdfc1c7d2918397a0cc0fe55bc31
anilcrk <36817127+anilcrk@users.noreply.github.com>
2020-06-03 02:37:23 +0300

    Add files via upload

---
+++ DemoFramework.Core/Constants/Messages/ExceptionMessages.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoFramework.Core.Constants.Messages
+{
+   public static class ExceptionMessages
+    {
+        public static string SystemExceptionMessage = "Runtime Error";
+        public static string IndexOutOfRangeExceptionMessage = "IndexOutOfRangeExceptionMessage";
+        public static string NullReferenceExceptionMessage = "NullReferenceException";
+        public static string AccessViolationExceptionMessage = "AccessViolationException";
+        public static string InvalidOperationExceptionMessage = "InvalidOperationExceptionMessage";
+        public static string ArgumentExceptionMessage = "ArgumentExceptionMessage";
+        public static string ArgumentNullExceptionMessage = "ArgumentNullExceptionMessage";
+        public static string ArgumentOutOfRangeExceptionMessage = "ArgumentOutOfRangeExceptionMessage";
+        public static string ExternalExceptionMessage = "ExternalExceptionMessage";
+        public static string COMExceptionMessage = "COMExceptionMessage";
+        public static string SEHExceptionMessage = "SEHExceptionMessage";
+        public static string ExceptionMessage = "ERRORR !!";
+
+        public static string SqlExceptionMessage = "SQL ERROR";
+        public static string DbUpdateExceptionMessage = "DATABASE UPDATE ERROR";
+    }
+}
